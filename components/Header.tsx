545c632860b5cdda8bd70d2c3fe4ed04878a7a5f
dyllanhiggins7@gmail.com <dyllanhiggins7@gmail.com>
2023-10-23 00:15:48 +0200

    Added Header and logo component

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import Logo from "./Logo"
+
+function Header() {
+  return (
+    <nav>
+        {/* Logo */}
+        <Logo />
+    </nav>
+  )
+}
+export default Header
---
+++ components/Logo.tsx
@@ -0,0 +1,9 @@
+import LogoImage from "@logos/chitChat_Logo_transparent.svg"
+import Link from "next/link"
+
+function Logo() {
+  return (
+    <Link href='/'>Go Home</Link>
+  )
+}
+export default Logo

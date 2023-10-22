c8f97e35884456026b296da72fa9850d8a831513
dyllanhiggins7@gmail.com <dyllanhiggins7@gmail.com>
2023-10-23 01:01:44 +0200

    Added web-app theme styling

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,9 +1,23 @@
 import LogoImage from "@logos/chitChat_Logo_transparent.svg"
 import Link from "next/link"
+import { AspectRatio } from "./ui/aspect-ratio"
+import Image from "next/image"
 
 function Logo() {
-  return (
-    <Link href='/'>Go Home</Link>
-  )
+  return <Link href="/" prefetch={false} className="overflow-hidden">
+    <div className="flex items-center w-72 h-14">
+        <AspectRatio 
+            ratio={16/9}
+            className="flex items-center justify-center"    
+        >
+            <Image
+                priority 
+                src={LogoImage} 
+                alt="ChitChat Logo"
+                className="dark:filter dark:invert"/>
+        </AspectRatio>
+        
+    </div>
+  </Link>
 }
-export default Logo
+export default Logo;
